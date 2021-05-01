ceb1d8a9037ee948121bc5fdcd959ec3f743ca18
Yernar <yernar.mussin@nitka.com>
2021-05-02 05:23:06 +0600

    added websocket

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 require('dotenv').config()
+const PORT = process.env.APP_PORT || 3012;
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const ws = require('ws')
+
+const wss = new ws.Server({
+    port: 5000
+}, () => console.log(`WebSocket started on 5000`))
+
+wss.on('connection', function connection(ws) {
+
+    ws.on('message', function(message) {
+
+        message = JSON.parse(message)
+
+        if (message.event === 'message') {
+            broadcastMessage(message)
+        } else if (message.event === 'connection') {
+            broadcastMessage(message)
+        }
+
+    })
+
+})
+
+function broadcastMessage(message) {
+    wss.clients.forEach(client => {
+        client.send(JSON.stringify(message))
+    })
+}
 
 const app = express()
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -12,8 +40,6 @@ app.use(cors())
 let router = require('./router')
 router.init(app)
 
-
-const PORT = process.env.APP_PORT || 3012;
 app.listen(PORT, () => {
     console.log(`Chart Server started on port ${PORT}`)
 })
