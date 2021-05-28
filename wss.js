93910043f2212e898693794334ee5624188a2170
Yernar <yernar.mussin@nitka.com>
2021-05-29 02:09:44 +0600

    wss

--- wss.js
+++ wss.js
@@ -7,37 +7,25 @@ const wss = new ws.Server({
     port: 5000
 }, () => console.log(`WebSocket started on ${WS_PORT}`))
 
-wss.on('connection', function connection(ws) {
-
-    ws.on('message', function(request) {
-
+wss.on('connection', (ws) => {
+    ws.on('message', (request) => {
         const requestMessage = JSON.parse(request)
-
         if (requestMessage.event === 'message' && requestMessage.user_id && requestMessage.message) {
-
             addMessage(requestMessage)
-
         } else if (requestMessage.event === 'connection') {
-
             addMessage(requestMessage)
-
             wsMessagesController.getMessages((err, res) => {
                 const messages = res && res.rows ? res.rows : []
                 ws.send(JSON.stringify({ event: 'get-all-messages', messages }))
             })
-
         }
-
     })
-
 })
 
 function addMessage(requestMessage) {
     const payload = { user_id: requestMessage.user_id, message: requestMessage.message, event: requestMessage.event }
     wsMessagesController.addMessageAndReturnWithColor(payload, (err, res) => {
-
         const responseMessage = res && res.rows && res.rows[0] ? res.rows[0] : null
-
         if (responseMessage && responseMessage.id) {
             const msg = {
                 id: responseMessage.id,
